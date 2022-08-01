1d40893d9fc7ab1a89972d8bcd678a5ff2986775
matthewfoxmi <matthewfoxmi@gmail.com>
2022-08-01 16:52:08 -0400

    Turning  In

--- country.js
+++ country.js
@@ -6,24 +6,79 @@ class Country {
         this.colors = colors;
     }
 }
-let usa = new Country("USA", "Murican", "Why Hello there world! Have some McDonalds", ["red", "white", "blue"]);
-let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["red", "white", "green"]);
-let algeria = new Country("Algeria", "Arabic and Berber", "Sup", ["blue", "yellow", "red"])
+let usa = new Country("USA", "English", "Hello world! Have some McDonalds", ["red", "white", "blue"]);
+let mexico = new Country("Mexico", "Spanish", "Hola mundo!", ["green", "white", "red"]);
+let algeria = new Country("Algeria", "Arabic and Berber", "marhaban bialealami!", ["blue", "yellow", "red"]);
+let japan = new Country("Japan", "Japanese", "Kon'nichiwa sekai!", ["red", "white"]);
+let russia = new Country("Russia", "Russian", "Privet, mir!", ["white", "blue", "red"]);
 
 
-function SwitchCountry() {
-    let input = "USA";
-    let country;
 
-    if (input === "USA") {
-        //set country to usa 
-        country = usa;
+    // for (let i = 0; i < Country.name.length; i++)
+    // {
+    //     console.log[i];
+    // }
+
+    // let CountryChoose = prompt(`What Country would you like to choose?  Your options are: ${i}`);
+    // alert(`${CountryChoose}`);
+
+//switching country properties    
+function SwitchCountry() 
+{
+    let countryNameNode = document.querySelector("#CountryName");
+    let countrycolor1Node = document.querySelector("#Color1");
+    let countrycolor2Node = document.querySelector("#Color2");
+    let countrycolor3Node = document.querySelector("#Color3");
+    let countryLangNode = document.querySelector("#OfficialLanguage");
+    let countryGreetingNode = document.querySelector("#HelloWorld");
+    let countryList = document.querySelector("#CountryList");
+
+    if (countryList.value === "USA") 
+    {
+        countryNameNode.innerText = usa.name;
+        countryLangNode.innerText = usa.lang;
+        countryGreetingNode.innerText = usa.greeting;
+        countrycolor1Node.style.backgroundColor = usa.colors[0];
+        countrycolor2Node.style.backgroundColor = usa.colors[1];
+        countrycolor3Node.style.backgroundColor = usa.colors[2];
+
     }
-    else if (input === "Mexico") {
-        country = mexico;
+    else if (countryList.value === "Mexico")
+    {        
+        countryNameNode.innerText = mexico.name;
+        countryLangNode.innerText = mexico.lang;
+        countryGreetingNode.innerText = mexico.greeting;
+        countrycolor1Node.style.backgroundColor = mexico.colors[0];
+        countrycolor2Node.style.backgroundColor = mexico.colors[1];
+        countrycolor3Node.style.backgroundColor = mexico.colors[2];        
     }
-    else if (input === "Algeria") {
-        country = algeria;
+    else if (countryList.value === "Algeria")
+    {
+        countryNameNode.innerText = algeria.name;
+        countryLangNode.innerText = algeria.lang;
+        countryGreetingNode.innerText = algeria.greeting;
+        countrycolor1Node.style.backgroundColor = algeria.colors[0];
+        countrycolor2Node.style.backgroundColor = algeria.colors[1];
+        countrycolor3Node.style.backgroundColor = algeria.colors[2];  
+    }
+    else if (countryList.value === "Japan") 
+    {
+        countryNameNode.innerText = japan.name;
+        countryLangNode.innerText = japan.lang;
+        countryGreetingNode.innerText = japan.greeting;
+        countrycolor1Node.style.backgroundColor = japan.colors[0];
+        countrycolor2Node.style.backgroundColor = japan.colors[1];
+        countrycolor3Node.style.backgroundColor = japan.colors[2];  
+    }
+    else if (countryList.value === "Russia") 
+    {
+        countryNameNode.innerText = russia.name;
+        countryLangNode.innerText = russia.lang;
+        countryGreetingNode.innerText = russia.greeting;
+        countrycolor1Node.style.backgroundColor = russia.colors[0];
+        countrycolor2Node.style.backgroundColor = russia.colors[1];
+        countrycolor3Node.style.backgroundColor = russia.colors[2];  
     }
 
+    //create logic "color1 = " so when choosing a new country and displaying, all coilorson screen should change.
 }
